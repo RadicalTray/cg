ab14388ee318876a6546ccfaee149f1f8fcde727
RadicalTray <radicaltray@gmail.com>
2025-04-28 02:57:29 +0700

    moving rain

--- src/main.cpp
+++ src/main.cpp
@@ -5,7 +5,9 @@
 #include "resources.h"
 
 Config parseArgs(int argc, char** argv);
+void update(Resources *resources);
 void draw(const Resources& resources, const int scr_width, const int scr_height);
+void drawRain(const Resources& resources);
 
 int main(int argc, char** argv) {
     Config config = parseArgs(argc, argv);
@@ -27,6 +29,7 @@ int main(int argc, char** argv) {
         int scr_width, scr_height;
         glfwGetFramebufferSize(window, &scr_width, &scr_height);
 
+        update(&resources);
         draw(resources, scr_width, scr_height);
 
         glfwSwapBuffers(window);
@@ -36,11 +39,34 @@ int main(int argc, char** argv) {
     windowDeinit(&window);
 }
 
+void update(Resources* resources) {
+    const float gravity = 0.01;
+    for (size_t i = 0; i < RAIN_VERTICES_COUNT; i += 4) {
+        RainVertex v0 = resources->rain_vertices[i];
+        RainVertex v1 = resources->rain_vertices[i+1];
+        RainVertex v2 = resources->rain_vertices[i+2];
+        RainVertex v3 = resources->rain_vertices[i+3];
+
+        v0.pos.y -= gravity;
+        v1.pos.y -= gravity;
+        v2.pos.y -= gravity;
+        v3.pos.y -= gravity;
+
+        resources->rain_vertices[i]   = v0;
+        resources->rain_vertices[i+1] = v1;
+        resources->rain_vertices[i+2] = v2;
+        resources->rain_vertices[i+3] = v3;
+    }
+    glBindBuffer(GL_ARRAY_BUFFER, resources->buffers.rain_vert_buf);
+    glBufferSubData(GL_ARRAY_BUFFER, 0, sizeof(resources->rain_vertices), resources->rain_vertices);
+    glBindBuffer(GL_ARRAY_BUFFER, 0);
+}
+
 void draw(const Resources& resources, const int scr_width, const int scr_height) {
-    Shaders shaders = resources.shaders;
-    Buffer buffer = resources.buffer;
-    GLuint image_texture = resources.texture;
-    RenderTarget render_target = resources.render_target;
+    const Shaders shaders = resources.shaders;
+    const Buffers buffers = resources.buffers;
+    const GLuint image_texture = resources.texture;
+    const RenderTarget render_target = resources.render_target;
 
     // to render target
     {
@@ -52,11 +78,14 @@ void draw(const Resources& resources, const int scr_width, const int scr_height)
 
         // render image
         glUseProgram(shaders.texture);
-        glBindVertexArray(buffer.vert_arr);
+        glBindVertexArray(buffers.vert_arr);
         glBindTexture(GL_TEXTURE_2D, image_texture);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)(0 * sizeof(GLuint)));
 
         // render rain
+        glUseProgram(resources.shaders.rain);
+        glBindVertexArray(resources.buffers.rain_vert_arr);
+        glDrawElements(GL_TRIANGLES, RAIN_INDICES_COUNT, GL_UNSIGNED_INT, 0);
     }
 
     // to window
@@ -69,7 +98,7 @@ void draw(const Resources& resources, const int scr_width, const int scr_height)
 
         glUseProgram(shaders.screen);
         glUniform2f(0, float(scr_height)/(scr_width), 1.0);
-        glBindVertexArray(buffer.vert_arr);
+        glBindVertexArray(buffers.vert_arr);
         glBindTexture(GL_TEXTURE_2D, render_target.texture);
         glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, (void*)(6 * sizeof(GLuint)));
     }
